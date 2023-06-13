bb62dcb7f65444e9e82af347c49675f8d4dbbbec
DearIce <56225620+DearIcer@users.noreply.github.com>
2023-06-13 19:12:29 +0800

    新增前端页面用户管理
    当前只能渲染数据

---
+++ RepositorySystemInterface/Controllers/UserInfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RepositorySystemInterface.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        // GET: UserInfo
+        public ActionResult ListView()
+        {
+            return View();
+        }
+    }
+}
